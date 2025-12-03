feeb36e1eed21614b61d5c34e9ba13d6e143b5d9
Mert Cahit Yiğit <yigitmertcahit@gmail.com>
2025-12-03 20:39:08 +0300

    I convert the data into tensors using PyTorch and split the data into training and testing sets.

--- test-tokenizer.py
+++ test-tokenizer.py
@@ -1,3 +1,5 @@
+import torch
+
 text = "merhaba dünya"
 chars = sorted(list(set(text)))
 
@@ -7,4 +9,10 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
-print(encode("merhaba"))
+data = torch.tensor(encode(text), dtype=torch.long)
+
+print(f"Toplam token sayısı: {data.shape[0]}")
+
+n = int(0.9 * len(data)) 
+train_data = data[:n]
+val_data = data[n:]
