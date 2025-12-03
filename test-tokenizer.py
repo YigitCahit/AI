cf020fbe5ed6b71aacf9c4e197a204cf1dd093d1
Mert Cahit Yiğit <yigitmertcahit@gmail.com>
2025-12-03 20:27:09 +0300

    To develop my own AI, I'm first trying to write a tokenizer.

---
+++ test-tokenizer.py
@@ -0,0 +1,10 @@
+text = "merhaba dünya"
+chars = sorted(list(set(text)))
+
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+print(encode("merhaba"))
