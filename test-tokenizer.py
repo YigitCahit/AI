6514240560ac144b677f5922776de33e301a3143
Mert Cahit Yiğit <yigitmertcahit@gmail.com>
2025-12-03 21:01:39 +0300

    I created a table filled with completely random numbers based on the model's Vocabulary size, meaning it was untrained, and I gave the model the first letter of the data, and the model made a prediction about what the next letter could be.

--- test-tokenizer.py
+++ test-tokenizer.py
@@ -1,7 +1,10 @@
 import torch
+import torch.nn as nn
 
 text = "merhaba dünya"
+
 chars = sorted(list(set(text)))
+vocab_size = len(chars)
 
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
@@ -16,3 +19,18 @@ print(f"Toplam token sayısı: {data.shape[0]}")
 n = int(0.9 * len(data)) 
 train_data = data[:n]
 val_data = data[n:]
+
+token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+print(f"Tablo (Weight) boyutu: {token_embedding_table.weight.shape}")
+
+idx = data[:1]
+
+print(f"\nGirdi Harf ID'si: {idx}") 
+print(f"Bu ID'nin harf karşılığı: '{decode(idx.tolist())}'")
+
+logits = token_embedding_table(idx)
+
+print(f"Çıktı Puanları (Logits) Boyutu: {logits.shape}")
+print(f"Açıklama: [1 (Girdi Sayısı), {vocab_size} (Olası her harf için puan)]")
+print(f"Üretilen ham puanlar (İlk 5 tanesi): {logits[0, :5]}")
